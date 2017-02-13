5ad53ec050381715e3bde7954c8dde519c141cd9
David Conner <agilebluefox@gmail.com>
2017-02-13 17:48:18 -0500

    Learn to use the shift operators

--- shift-operators.js
+++ shift-operators.js
@@ -7,12 +7,15 @@
  */
 function shift(value, bit) {
     var shiftedLeft = value << bit;
-    var shiftedRight = value >> bit;
+    var shiftedRight = value >> bit; // The sign bit determines whether the fill is 1 or 0
+    var zeroShift = value >>> bit; // Always shift right using 0
     console.log("The value " + value + " becomes " + shiftedLeft + " when shifted left");
     console.log("The value " + value + " becomes " + shiftedRight + " when shifted right");
+    console.log("The value " + value + " becomes " + zeroShift + " when zero shifted right");
     return {
         'left': shiftedLeft,
-        'right': shiftedRight
+        'right': shiftedRight,
+        'zero': zeroShift
     };
 }
 /**
@@ -31,9 +34,98 @@ function checkLeftValue(shifted, value, bit) {
         console.log("The left shifted calculation should be " + calcLeftShift);
     }
 }
-// Shift the number 23 to the left 5 bits
+// Shift the number -23 to the left and right 5 bits
+console.log("\nShift the number -23:");
 var value = -23;
 var bit = 5;
 var number = shift(value, bit);
 checkLeftValue(number, value, bit);
+// Shift the number 11 to the left and right 4 bits
+console.log("\nShift the number 11:");
+value = 11;
+bit = 4;
+number = shift(value, bit);
+checkLeftValue(number, value, bit);
+/**
+ * Doubles an integer
+ *
+ * @param {Number} number The number to double
+ * @returns {Number} The double value of the number
+ */
+function doubleValue(number) {
+    var double = number << 1;
+    console.log("The integer " + number + " doubled is " + double);
+    return double;
+}
+// Double the number 32
+console.log("\nDouble the number 32:");
+value = 32;
+var double = doubleValue(value);
+/**
+ * Quadruples an integer
+ *
+ * @param {Number} number The number to quadruple
+ * @returns {Number} The quadruple of the mumber
+ */
+function quadrupleValue(number) {
+    var quad = number << 2;
+    console.log("The integer " + number + " quadrupled is " + quad);
+    return quad;
+}
+// Quadruple the number 32
+console.log("\nQuadruple the number 32:");
+value = 32;
+var quad = quadrupleValue(value);
+function divideByTwo(number) {
+    var value = number >> 1;
+    console.log("The number " + number + " divided by two rounded down is " + value);
+    return value;
+}
+// Divide the number 32 by two
+console.log("Divide the number 32 by two:");
+value = 32;
+var dividedByTwo = divideByTwo(value);
+function powersOfTwo(exp) {
+    var number = 1 << exp;
+    console.log("Two raised to the " + exp + " power is " + number);
+    return number;
+}
+// Powers of two
+console.log("\nCalculate 2^n");
+var exp = 5;
+var raised = powersOfTwo(5);
+/**
+ * Calculates the Morton Number for two 8-bit values (0 - 511)
+ *
+ * @param {Number} num1
+ * @param {Number} num2
+ * @returns {Number} The Morton number for the two values
+ */
+function mortonfyIt(num1, num2) {
+    // shift the eigth bit of the first number to pos 16
+    // shift the eigth bit of the second number to pos 15
+    var morton = 0;
+    var exp = 0;
+    for (var i = 7; i >= 0; i--) {
+        console.log("i: " + i);
+        var mask = Math.pow(2, i);
+        console.log("The current mask is: " + mask);
+        exp = i * 2;
+        if (num1 & mask) {
+            morton += Math.pow(2, (exp + 1));
+            console.log("The current exponent is: " + (exp + 1));
+            console.log("The current morton number is: " + morton);
+        }
+        if (num2 & mask) {
+            morton += Math.pow(2, (exp));
+            console.log("The current exponent is: " + exp);
+            console.log("The current morton number is: " + morton);
+        }
+    }
+    console.log("The morton number for " + num1 + " and " + num2 + " is " + morton);
+    return morton;
+}
+// Morton Number
+console.log("\nCalculate the Morton Number:");
+var num = mortonfyIt(15, 48);
 //# sourceMappingURL=shift-operators.js.map
--- shift-operators.ts
+++ shift-operators.ts
@@ -7,12 +7,15 @@
  */
 function shift(value, bit) {
     let shiftedLeft = value << bit;
-    let shiftedRight = value >> bit;
+    let shiftedRight = value >> bit; // The sign bit determines whether the fill is 1 or 0
+    let zeroShift = value >>> bit; // Always shift right using 0
     console.log(`The value ${value} becomes ${shiftedLeft} when shifted left`);
     console.log(`The value ${value} becomes ${shiftedRight} when shifted right`);
+    console.log(`The value ${value} becomes ${zeroShift} when zero shifted right`);
     return {
         'left': shiftedLeft,
-        'right': shiftedRight
+        'right': shiftedRight,
+        'zero': zeroShift
     };
 }
 
@@ -32,8 +35,108 @@ function checkLeftValue(shifted, value, bit) {
     }
 }
 
-// Shift the number 23 to the left 5 bits
+// Shift the number -23 to the left and right 5 bits
+console.log(`\nShift the number -23:`);
 let value = -23;
 let bit = 5;
 let number = shift(value, bit);
 checkLeftValue(number, value, bit);
+
+// Shift the number 11 to the left and right 4 bits
+console.log(`\nShift the number 11:`);
+value = 11;
+bit = 4;
+number = shift(value, bit);
+checkLeftValue(number, value, bit);
+
+/**
+ * Doubles an integer
+ * 
+ * @param {Number} number The number to double
+ * @returns {Number} The double value of the number
+ */
+function doubleValue(number) {
+    let double = number << 1;
+    console.log(`The integer ${number} doubled is ${double}`);
+    return double;
+}
+
+// Double the number 32
+console.log(`\nDouble the number 32:`);
+value = 32;
+let double = doubleValue(value);
+
+/**
+ * Quadruples an integer
+ * 
+ * @param {Number} number The number to quadruple
+ * @returns {Number} The quadruple of the mumber
+ */
+function quadrupleValue(number) {
+    let quad = number << 2;
+    console.log(`The integer ${number} quadrupled is ${quad}`);
+    return quad;
+}
+
+// Quadruple the number 32
+console.log(`\nQuadruple the number 32:`);
+value = 32;
+let quad = quadrupleValue(value);
+
+function divideByTwo(number) {
+    let value = number >> 1;
+    console.log(`The number ${number} divided by two rounded down is ${value}`);
+    return value;
+}
+
+// Divide the number 32 by two
+console.log(`\Divide the number 32 by two:`);
+value = 32;
+let dividedByTwo = divideByTwo(value);
+
+function powersOfTwo(exp) {
+    let number = 1 << exp;
+    console.log(`Two raised to the ${exp} power is ${number}`);
+    return number;
+}
+
+// Powers of two
+console.log(`\nCalculate 2^n`);
+let exp = 5;
+let raised = powersOfTwo(5);
+
+/**
+ * Calculates the Morton Number for two 8-bit values (0 - 511)
+ * 
+ * @param {Number} num1
+ * @param {Number} num2
+ * @returns {Number} The Morton number for the two values
+ */
+function mortonfyIt(num1, num2) {
+    // shift the eigth bit of the first number to pos 16
+    // shift the eigth bit of the second number to pos 15
+    let morton = 0;
+    let exp = 0;
+    for (let i = 7; i >= 0; i--) {
+        console.log(`i: ${i}`);
+        let mask = 2 ** i;
+        console.log(`The current mask is: ${mask}`);
+        exp = i * 2;
+        if (num1 & mask) {
+            morton += 2 ** (exp + 1);
+            console.log(`The current exponent is: ${exp + 1}`);
+            console.log(`The current morton number is: ${morton}`);
+        }
+        if (num2 & mask) {
+            morton += 2 ** (exp);
+            console.log(`The current exponent is: ${exp}`);
+            console.log(`The current morton number is: ${morton}`);
+        }
+    }
+    console.log(`The morton number for ${num1} and ${num2} is ${morton}`);
+    return morton;
+}
+
+// Morton Number
+console.log(`\nCalculate the Morton Number:`);
+let num = mortonfyIt(15, 48);
