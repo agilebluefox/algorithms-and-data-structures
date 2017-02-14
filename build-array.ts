8350e3536734605151f91a5fad2a53bc61a3b001
David Conner <agilebluefox@gmail.com>
2017-02-14 09:05:58 -0500

    Optimize an array
    To improve the performance of an array, it can help to set aside more memory initially than required. When additional items are added to the array, the memory is already allocated.

--- build-array.ts
+++ build-array.ts
@@ -2,14 +2,25 @@
 import memory = require('./memory');
 
 // Build an array of zero length to represent memory
+// The capacity represents the available space allocated
 const Array = function() {
     this.length = 0;
+    this._capacity = 0;
     this.ptr = memory.allocate(this.length);
 };
 
+// Factor by which to allocate more space than you currently need
+// This can optimize the array to O(1) although more memory is
+// temporarily lost.
+
+Array.SIZE_RATIO = 3;
+
 // The push method takes a value, makes a place for it, then stores it
+// To accommodate more items, the resize uses a factor
 Array.prototype.push = function(value) {
-    this._resize(this.length + 1);
+    if (this.length >= this._capacity) {
+        this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
     memory.set(this.ptr + this.length, value);
     this.length++;
 };
@@ -23,4 +34,6 @@ Array.prototype._resize = function(size) {
     }
     memory.copy(this.ptr, oldPtr, this.length);
     memory.free(oldPtr);
+    // Update the capacity to the allocated memory
+    this._capacity = size;
 };
