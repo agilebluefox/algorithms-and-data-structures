47dfd33fd9a8affd46304c54cef4fd0af2bdedca
David Conner <agilebluefox@gmail.com>
2017-02-15 14:14:26 -0500

    Merge and sort two sorted arrays

--- array-questions.js
+++ array-questions.js
@@ -19,4 +19,73 @@ var myArray = [1, 4, 5, 8, 6, 5, 7, 3, 5, 6, 9, 7];
 console.log("The original array: ", myArray);
 var filteredArray = removeLessThanFive(myArray);
 console.log("The filtered array: ", filteredArray);
+/**
+ * Take two sorted arrays and combine them into a single sorted array
+ *
+ * @param {Array} array1 - The first sorted array
+ * @param {Array} array2 - The second sorted array
+ * @return {Array} A single sorted and merged array
+ */
+function mergeSortArray(array1, array2) {
+    // The merged array
+    var merged = [];
+    // Starting indexes for each array 
+    var x = 0;
+    var y = 0;
+    // Compare the values of each array and store the lowest number
+    // in the merged array
+    do {
+        // Value 1 is less than value 2
+        if (array1[x] < array2[y]) {
+            // Push value 1 to the merged array
+            merged.push(array1[x]);
+            console.log("X is " + x + ", Y is " + y + ": ", merged);
+            // Go to the next value in the first array
+            x += 1;
+        }
+        else if (array1[x] === array2[y]) {
+            // Push value 1 to the merged array
+            merged.push(array1[x]);
+            console.log("X is " + x + ", Y is " + y + ": ", merged);
+            // Go to the next value in the first array
+            x += 1;
+        }
+        else if (array1[x] > array2[y]) {
+            // Push value 2 to the merged array
+            merged.push(array2[y]);
+            console.log("X is " + x + ", Y is " + y + ": ", merged);
+            // Go to the next value in the second array
+            y += 1;
+        }
+        // Check to see if one of the arrays is out of values
+        // If so, just keep pushing the values from the other
+        // array to the merged array
+        if (array1[x] && y >= array2.length) {
+            merged.push(array1[x]);
+            console.log("X is " + x + ", Y is " + y + ": ", merged);
+            x += 1;
+        }
+        if (array2[y] && x >= array1.length) {
+            merged.push(array2[y]);
+            console.log("X is " + x + ", Y is " + y + ": ", merged);
+            y += 1;
+        }
+    } while (array1[x] || array2[y]);
+    // Return the merged and sorted array
+    return merged;
+}
+var array1 = [1, 3, 6, 8, 11];
+var array2 = [2, 3, 5, 8, 9, 10];
+console.log("\n***** Merge two sorted arrays into a single sorted array *****\n");
+var mergedArray = mergeSortArray(array1, array2);
+console.log("The first array: ", array1);
+console.log("The second array: ", array2);
+console.log("The merged array: ", mergedArray);
+array1 = [1, 3, 6, 8, 11, 45, 63];
+array2 = [2, 3, 5, 8, 9, 10];
+console.log("***** Merge two sorted arrays into a single sorted array *****\n");
+mergedArray = mergeSortArray(array1, array2);
+console.log("The first array: ", array1);
+console.log("The second array: ", array2);
+console.log("The merged array: ", mergedArray);
 //# sourceMappingURL=array-questions.js.map
--- array-questions.ts
+++ array-questions.ts
@@ -20,3 +20,84 @@ const myArray = [1, 4, 5, 8, 6, 5, 7, 3, 5, 6, 9, 7];
 console.log(`The original array: `, myArray);
 let filteredArray = removeLessThanFive(myArray);
 console.log(`The filtered array: `, filteredArray);
+
+/**
+ * Take two sorted arrays and combine them into a single sorted array
+ * 
+ * @param {Array} array1 - The first sorted array
+ * @param {Array} array2 - The second sorted array
+ * @return {Array} A single sorted and merged array
+ */
+function mergeSortArray(array1: number[], array2: number[]) {
+    // The merged array
+    let merged: number[] = [];
+
+    // Starting indexes for each array 
+    let x = 0;
+    let y = 0;
+
+    // Compare the values of each array and store the lowest number
+    // in the merged array
+    do {
+        // Value 1 is less than value 2
+        if (array1[x] < array2[y]) {
+            // Push value 1 to the merged array
+            merged.push(array1[x]);
+            console.log(`X is ${x}, Y is ${y}: `, merged);
+            // Go to the next value in the first array
+            x += 1;
+            // Value 1 is equal to value 2
+        } else if (array1[x] === array2[y]) {
+            // Push value 1 to the merged array
+            merged.push(array1[x]);
+            console.log(`X is ${x}, Y is ${y}: `, merged);
+            // Go to the next value in the first array
+            x += 1;
+            // Value 2 is less than value 1
+        } else if (array1[x] > array2[y]) {
+            // Push value 2 to the merged array
+            merged.push(array2[y]);
+            console.log(`X is ${x}, Y is ${y}: `, merged);
+            // Go to the next value in the second array
+            y += 1;
+        }
+
+        // Check to see if one of the arrays is out of values
+        // If so, just keep pushing the values from the other
+        // array to the merged array
+        if (array1[x] && y >= array2.length) {
+            merged.push(array1[x]);
+            console.log(`X is ${x}, Y is ${y}: `, merged);
+            x += 1;
+        }
+        if (array2[y] && x >= array1.length) {
+            merged.push(array2[y]);
+            console.log(`X is ${x}, Y is ${y}: `, merged);
+            y += 1;
+        }
+    }
+    // As long as one of the arrays contains a valid value keep iterating
+    while (array1[x] || array2[y]);
+
+    // Return the merged and sorted array
+    return merged;
+}
+
+let array1 = [1, 3, 6, 8, 11];
+let array2 = [2, 3, 5, 8, 9, 10];
+
+console.log(`\n***** Merge two sorted arrays into a single sorted array *****\n`);
+let mergedArray = mergeSortArray(array1, array2);
+console.log(`The first array: `, array1);
+console.log(`The second array: `, array2);
+console.log(`The merged array: `, mergedArray);
+
+
+array1 = [1, 3, 6, 8, 11, 45, 63];
+array2 = [2, 3, 5, 8, 9, 10];
+
+console.log(`***** Merge two sorted arrays into a single sorted array *****\n`);
+mergedArray = mergeSortArray(array1, array2);
+console.log(`The first array: `, array1);
+console.log(`The second array: `, array2);
+console.log(`The merged array: `, mergedArray);
