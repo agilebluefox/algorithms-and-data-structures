17a30a199ec89c30a9e727413bc37923e9e61990
David Conner <agilebluefox@gmail.com>
2017-02-22 13:50:48 -0500

    Outline hash map class
    Provide basic outline including the properties and methods required to design a hash map implementation using separate chaining.

---
+++ hash-map-chaining.ts
@@ -0,0 +1,27 @@
+export class HashMapChained {
+    // Basic parameters for the hash map
+    private _slots: any[] = [];
+    private capacity: number = 8;
+
+    // method to insert data
+    insert() {
+
+    }
+
+    // method to retrieve data
+    get() {
+
+    }
+
+    // method to delete data
+    remove() {
+
+    }
+
+    // method to find a slot
+    _findSlot() {
+        
+    }
+
+    // 
+}
